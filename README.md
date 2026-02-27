c1bc6a0e91affb5acc1b61b098d5264d7c492e36
usershokhrukh <asxurovsit@gmail.com>
2026-02-27 21:07:03 +0500

    rosalind https://rosalind.info/problems/splc/ solved

--- README.md
+++ README.md
@@ -1,13 +1,83 @@
-# Locating Restriction Sites
+# RNA Splicing
 
 ### Problem Description
-
-We should compare the given DNA string with **reversed complement**, if they match, then print order of **palindrome** before length of it
-
-In biology, bioinformaticians look for these sequences to identify where DNA can be cut, the "molecular scissors" called **Restriction Enzymes** help to avoid folding like "hairpin"
+```
+rna_codon_table = {
+    # U bilan boshlanadiganlar
+    "UUU": "F",
+    "UUC": "F",
+    "UUA": "L",
+    "UUG": "L",
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "UAU": "Y",
+    "UAC": "Y",
+    "UAA": "Stop",
+    "UAG": "Stop",
+    "UGU": "C",
+    "UGC": "C",
+    "UGA": "Stop",
+    "UGG": "W",
+    # C bilan boshlanadiganlar
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "CAU": "H",
+    "CAC": "H",
+    "CAA": "Q",
+    "CAG": "Q",
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",# A bilan boshlanadiganlar
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+    "AUG": "M",
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "AAU": "N",
+    "AAC": "N",
+    "AAA": "K",
+    "AAG": "K",
+    "AGU": "S",
+    "AGC": "S",
+    "AGA": "R",
+    "AGG": "R",
+    # G bilan boshlanadiganlar
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "GAU": "D",
+    "GAC": "D",
+    "GAA": "E",
+    "GAG": "E",
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+}
+```
+In first part we have a DNA string, on the other lines there are **introns** which we should replace in DNA and leave only **exons**, result would be DNA string in existence exons.
+After all, we must transcript to **RNA** then translate to proteins, so after transcription, it matches with codons
 
 ### Mathematical Logic
 
-1. **Read DNA string** 
-2. Check every **4-12** sequence with revp
-3. print matched strings with order and length
+1. **Read FASTA file** 
+2. **remove introns in DNA** 
+3. **Turn DNA exon string to RNA**
+4. **Math RNA codons into proteins**
--- project.py
+++ project.py
@@ -1,18 +1,111 @@
-def analyze_revp(seq):
-    matching = {"A": "T", "T": "A", "C": "G", "G": "C"}
-    reversedSeq = "".join(matching.get(i) for i in reversed(seq));
-    return seq == reversedSeq;
+rna_codon_table = {
+    # U bilan boshlanadiganlar
+    "UUU": "F",
+    "UUC": "F",
+    "UUA": "L",
+    "UUG": "L",
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "UAU": "Y",
+    "UAC": "Y",
+    "UAA": "Stop",
+    "UAG": "Stop",
+    "UGU": "C",
+    "UGC": "C",
+    "UGA": "Stop",
+    "UGG": "W",
+    # C bilan boshlanadiganlar
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "CAU": "H",
+    "CAC": "H",
+    "CAA": "Q",
+    "CAG": "Q",
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",# A bilan boshlanadiganlar
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+    "AUG": "M",
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "AAU": "N",
+    "AAC": "N",
+    "AAA": "K",
+    "AAG": "K",
+    "AGU": "S",
+    "AGC": "S",
+    "AGA": "R",
+    "AGG": "R",
+    # G bilan boshlanadiganlar
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "GAU": "D",
+    "GAC": "D",
+    "GAA": "E",
+    "GAG": "E",
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+}
 
-def main_revp(data):
-    for i in range(len(data)):
-        for length in range(4, 13):
-            if(i+length <= len(data)):
-                seq = data[i:i+length];
-                result = analyze_revp(seq);
-                if(result):
-                    print(f"{i+1} {len(seq)}")
 
-with open("rosalind_revp.txt", "r") as file:
-    data = file.readlines()
-    seq = "".join(line.strip() for line in data[1:])
-    main_revp(seq)
+def splicing(DNA, introns):
+    exons = DNA
+    for miniIntrons in introns:
+        exons = exons.replace(miniIntrons, "")
+    exons = exons.replace("T", "U")
+    protein = "".join(
+        rna_codon_table.get(exons[codon : codon + 3])
+        for codon in range(0, len(exons), 3)
+        if rna_codon_table.get(exons[codon : codon + 3]) != "Stop"
+    )
+    return protein
+
+
+def read_fasta(file_path):
+    sequences = {}
+    current_id = None
+
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
+            if line.startswith(">"):
+                current_id = line[1:] 
+                sequences[current_id] = ""
+            else:
+                sequences[current_id] += line
+
+    return sequences
+
+
+data = read_fasta("rosalind_splc.txt")
+
+with open("rosalind_splc.txt", "r") as file:
+    ros = [line[1:].strip() for line in file.readlines() if line.startswith(">")]
+    DNA = data[ros[0]]
+    introns = [data[line.strip()] for line in ros[1:]]
+    result = splicing(DNA, introns)
+    print(result)
